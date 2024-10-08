fbe3cbd7e4b1dd51c142b6d8a218b45de956cf39
Sarath Jayakumar <sarathjayakumar46@gmail.com>
2024-10-08 16:24:39 +0000

    models like Category,Tag,Customer,Product are created

---
+++ vrindapots/store/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+import datetime
+# Create your models here.
+
+# Categories of products
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+# All the products
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, default=1)
+    new_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    old_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank= True, null= True)
+    image1 = models.ImageField(upload_to='uploads/product/')
+    image2 = models.ImageField(upload_to='uploads/product/')
+    image3 = models.ImageField(upload_to='uploads/product/')
+
+
+    def __str__(self):
+        return self.name
+
+
+
+# class Order(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+#     quantity = models.IntegerField(default=1)
+#     address =
+#     phone =
+#     date =
+#     status =
+
+#     def __str__(self):
+#         return self.name
