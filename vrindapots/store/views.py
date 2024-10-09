7ed4a334ad8695318edd50f3251f4b3971b8bd03
Sarath Jayakumar <sarathjayakumar46@gmail.com>
2024-10-09 05:39:10 +0000

    exclusive section added in home page

--- vrindapots/store/views.py
+++ vrindapots/store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Category,Product,Tag
+from authentication.models import Customer
 # Create your views here.
 
 def home_page(request):
-    return render(request,'home.html')
+
+    Exclusive_Offer_products = Product.objects.filter(tag__name='Exclusive Offers')
+
+    return render(request,'home.html',{'Exclusive_Offer_products': Exclusive_Offer_products})
