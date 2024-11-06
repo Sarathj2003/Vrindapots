415eacd2d74806537d4d596c04ab9b73b22f4a40
Sarath Jayakumar <sarathjayakumar46@gmail.com>
2024-11-06 13:04:48 +0000

    created models for wishlist

--- vrindapots/store/models.py
+++ vrindapots/store/models.py
@@ -67,7 +67,16 @@ class Product(models.Model):
         return None  
     
 
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlists')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='wishlisted_by')
+    added_on = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('user', 'product')
+
+    def __str__(self):
+        return f"{self.user.username}'s wishlist item: {self.product.name}"
 
 
 
